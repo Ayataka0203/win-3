bf8f01d0049e722041771b9b4d78a1165af21263
Ayataka0203 <93465737+Ayataka0203@users.noreply.github.com>
2022-01-24 00:45:39 +0900

    win3 main

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,14 @@ import Main1 from "./Main1";
 import Main2 from "./Main2";
 import Main3 from "./Main3";
 import Main4 from "./Main4";
+import Main6 from "./Main6";
+import Main7 from "./Main7";
+import Main8 from "./Main8";
 import Footer from "./Footer";
 //コメントアウト　　　　　　{/*  コメント　　　　　　*/}
+//プロフィール画面：Header+Main1+Main2+Footer
+//作品閲覧画面：Header+Main3+Main4+Footer
+//ホーム画面：：Header+Main5+Main6+Main7+Main8+Footer
 
 class App extends React.Component {
   render() {
@@ -19,12 +25,12 @@ class App extends React.Component {
           <p>
             <Main1 />
           </p>
-        */}
-        {/*
+        }
+        {
           <p>
             <Main2 />
           </p>
-        */}
+        }
         {
           <p>
             <Main3 />
@@ -33,6 +39,21 @@ class App extends React.Component {
         {
           <p>
             <Main4 />
+          </p>
+        */}
+        {
+          <p>
+            <Main6 />
+          </p>
+        }
+        {
+          <p>
+            <Main7 />
+          </p>
+        }
+        {
+          <p>
+            <Main8 />
           </p>
         }
         {
