df0ccf54d08023bcd61275fee9225a5b9776568a
Ayataka0203 <93465737+Ayataka0203@users.noreply.github.com>
2022-01-24 16:23:33 +0900

    1/24 16:23

--- src/components/Main10.js
+++ src/components/Main10.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import Stack from "@mui/material/Stack";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Input = styled("input")({
   display: "none"
@@ -50,17 +49,12 @@ class Main10 extends React.Component {
         </div>
         <br></br>
         <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
         <p
           style={{
             marginLeft: "13px"
           }}
         >
-          作品タイトル
+          ペンネーム
         </p>
         <Box
           sx={{
@@ -68,14 +62,14 @@ class Main10 extends React.Component {
             maxWidth: "100%"
           }}
         >
-          <TextField fullWidth label="作品タイトルを入力" id="fullWidth" />
+          <TextField fullWidth label="ペンネームを入力" id="fullWidth" />
         </Box>
         <p
           style={{
             marginLeft: "13px"
           }}
         >
-          キャプション
+          ポートフォリオ
         </p>
         <Box
           sx={{
@@ -83,14 +77,14 @@ class Main10 extends React.Component {
             maxWidth: "100%"
           }}
         >
-          <TextField fullWidth label="キャプションを入力" id="fullWidth" />
+          <TextField fullWidth label="ポートフォリオURLを入力" id="fullWidth" />
         </Box>
         <p
           style={{
             marginLeft: "13px"
           }}
         >
-          タグ
+          Twitter
         </p>
         <Box
           sx={{
@@ -98,111 +92,8 @@ class Main10 extends React.Component {
             maxWidth: "100%"
           }}
         >
-          <TextField fullWidth label="タグを追加" id="fullWidth" />
+          <TextField fullWidth label="Twitter IDを入力" id="fullWidth" />
         </Box>
-
-        <div className="syurui">
-          <FormControl>
-            <FormLabel id="demo-radio-buttons-group-label">
-              作品の種類
-            </FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="female"
-              name="radio-buttons-group"
-            >
-              <FormControlLabel
-                value="female"
-                control={<Radio />}
-                label="イラスト"
-              />
-              <FormControlLabel value="male" control={<Radio />} label="映像" />
-              <FormControlLabel
-                value="other"
-                control={<Radio />}
-                label="コード"
-              />
-            </RadioGroup>
-          </FormControl>
-        </div>
-
-        <div className="seigen">
-          <FormControl>
-            <FormLabel id="demo-radio-buttons-group-label">閲覧制限</FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="female"
-              name="radio-buttons-group"
-            >
-              <FormControlLabel
-                value="female"
-                control={<Radio />}
-                label="全年齢可"
-              />
-              <FormControlLabel value="male" control={<Radio />} label="R-18" />
-              <FormControlLabel
-                value="other"
-                control={<Radio />}
-                label="R-18G"
-              />
-            </RadioGroup>
-          </FormControl>
-        </div>
-
-        <div className="hani">
-          <FormControl>
-            <FormLabel id="demo-radio-buttons-group-label">公開設定</FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="female"
-              name="radio-buttons-group"
-            >
-              <FormControlLabel
-                value="female"
-                control={<Radio />}
-                label="公開"
-              />
-              <FormControlLabel
-                value="male"
-                control={<Radio />}
-                label="非公開"
-              />
-            </RadioGroup>
-          </FormControl>
-        </div>
-
-        <div className="seiteki">
-          <FormControl>
-            <FormLabel id="demo-radio-buttons-group-label">性的描写</FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="female"
-              name="radio-buttons-group"
-            >
-              <FormControlLabel
-                value="female"
-                control={<Radio />}
-                label="なし"
-              />
-              <FormControlLabel
-                value="male"
-                control={<Radio />}
-                label="あり（軽度）"
-              />
-              <FormControlLabel
-                value="other"
-                control={<Radio />}
-                label="あり（重度）"
-              />
-            </RadioGroup>
-          </FormControl>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-        </div>
       </div>
     );
   }
