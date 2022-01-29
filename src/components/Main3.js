48e2bd7660784c4b95b4eb56880f30a84b525166
Ayataka0203 <93465737+Ayataka0203@users.noreply.github.com>
2022-01-29 13:18:34 +0900

    1/29 13:18　更新

--- src/components/Main3.js
+++ src/components/Main3.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Main1 from "./Main1";
 import Main4 from "./Main4";
 import Button from "@material-ui/core/Button";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
@@ -6,32 +7,40 @@ import ArticleIcon from "@mui/icons-material/Article";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-class Main3 extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="main3">
-          <div className="prof-left">
-            <div class="box box6">picture</div>
-          </div>
-          <ul className="task">
-            <div style={{ fontSize: "40px" }}>Summer sea</div>
-            <div className="icons4">
-              <FavoriteBorderIcon />
-              <ArticleIcon />
+const Main3 = () => {
+  const [taskFlag, setTaskFlag] = useState(true);
+  const [profFlag, setProfFlag] = useState(false);
+  const onClickProf = () => {
+    setProfFlag(!profFlag);
+    setTaskFlag(!taskFlag);
+  };
+
+  return (
+    <>
+      {taskFlag && (
+        <>
+          <div className="main3">
+            <div className="prof-left">
+              <div class="box box6">picture</div>
             </div>
-            <Button color="primary" variant="contained">
-              by Ayataka0203
-            </Button>
-            <p>作品解説</p>
-          </ul>
-        </div>
-        <p>
+            <ul className="task">
+              <div style={{ fontSize: "40px" }}>Summer sea</div>
+              <div className="icons4">
+                <FavoriteBorderIcon />
+                <ArticleIcon />
+              </div>
+              <Button color="primary" variant="contained" onClick={onClickProf}>
+                by Ayataka0203
+              </Button>
+              <p>作品解説</p>
+            </ul>
+          </div>
           <Main4 />
-        </p>
-      </>
-    );
-  }
-}
+        </>
+      )}
+      {profFlag && <Main1 />}
+    </>
+  );
+};
 
 export default Main3;
